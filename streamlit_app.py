cdcef04bf9ca39e987148d21beca74a18d1b9ed6
Shiam David Mohammed <56936685+shiamdavid@users.noreply.github.com>
2022-11-06 22:35:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ streamlit.dataframe(my_data_rows)
 
 #allow the end user to add a fruit to the list
 add_my_fruit=streamlit.text_input('What fruit would you like to add?')
-back_from_function=insert_row_snowflake(add_my_fruit)
+insert_row_snowflake(add_my_fruit)
 streamlit.write('Thanks for adding ', add_my_fruit) 
 
 
